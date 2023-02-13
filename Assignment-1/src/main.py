ede985a11a0d1092d4c146b0cf9b0955575e380a
Pratyush Saini <66582958+pratyushiitd@users.noreply.github.com>
2023-02-13 16:59:39 +0530

    best accuracy so far

--- Assignment-1/src/main.py
+++ Assignment-1/src/main.py
@@ -4,9 +4,9 @@ from imports import *
 def parse_args():
     parser = argparse.ArgumentParser(description='arguements for the text classifier')
     parser.add_argument('--train_file', type=str, default='train.csv', help='path to training data')
-    parser.add_argument('--lemmatize', type=int, default=True, help='lemmatize the text')
-    parser.add_argument('--stem', type=int, default=True, help='stem the text')
-    parser.add_argument('--remove_stopwords', type=int, default=True, help='remove stop words')
+    parser.add_argument('--lemmatize', type=int, default=1, help='lemmatize the text')
+    parser.add_argument('--stem', type=int, default=0, help='stem the text')
+    parser.add_argument('--remove_stopwords', type=int, default=1, help='remove stop words')
     parser.add_argument('--out_file', type=str, default='train_processed.csv', help='path to processed training data')
     parser.add_argument('--random_state', type=int, default=42, help='random state for train test split')
     parser.add_argument('--test_size', type=float, default=0.2, help='test size for train test split')
@@ -14,6 +14,7 @@ def parse_args():
     parser.add_argument('--ngrams', type=int, default=1, help='ngrams to use for the model')
     parser.add_argument('--min_df', type=float, default=1, help='min_df to use for the model')
     parser.add_argument('--max_df', type=float, default=1.0, help='max_df to use for the model')
+    parser.add_argument('--tfidf_max_feat', type=int, default=60000, help='max_features to use for the model')
     return parser
 
 class TextPreprocessor():
@@ -37,16 +38,21 @@ class TextPreprocessor():
         print(self.train_df.head())
         if (self.remove_stopwords): self.remove_stopwords_(self.train_df)
         print(self.train_df.head())
-    
+        self.train_df["reviews"] = self.train_df["reviews"].str.replace('[^\w\s]','')
+
+        tfidf = TfidfVectorizer(max_features=60000, ngram_range=(1,2), use_idf=True, norm='l2', smooth_idf=True, sublinear_tf=True)
+        X = tfidf.fit_transform(self.train_df["reviews"])
+        print("TFIDF done")
+        return X, self.train_df["labels"]
     def lemmatize_text(self, train_df):
         train_df["reviews"] = train_df["reviews"].apply(lambda x: " ".join([self.lemmatizer.lemmatize(word) for word in x.split()]))
-
+        print("Lemmatized")
     def stem_text(self, train_df):
         train_df["reviews"] = train_df["reviews"].apply(lambda x: " ".join([self.stemmer.stem(word) for word in x.split()]))
-
+        print("Stemmed")
     def remove_stopwords_(self, train_df):
         train_df["reviews"] = train_df["reviews"].apply(lambda x: " ".join([word for word in x.split() if word not in self.stopwords]))
-    
+        print("Removed stopwords")
 class DataLoader:
     def __init__(self, args):
         self.args = args
@@ -56,28 +62,20 @@ class DataLoader:
         df.dropna(axis = 0, inplace=True)
         return df
     
-    def test_train_split(self, train_df, test_size=0.2):
-        X_train, X_test, y_train, y_test = train_test_split(train_df['reviews'], train_df['labels'], test_size=self.args.test_size, random_state=self.args.random_state)
+    def test_train_split(self, X, y, test_size=0.2):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=self.args.test_size, random_state=self.args.random_state)
 
-        weights = pd.Series(y_train).value_counts().reset_index()
-        weights.columns = ['label', 'counts']
-        weights['weights'] = weights['counts'].sum() / weights['counts']
-        weights = weights.set_index('label')['weights'].to_dict()
-        sample_weights = pd.Series(y_train).map(weights)
-
-        return X_train, X_test, y_train, y_test, sample_weights
+        return X_train, X_test, y_train, y_test
 
 class Classifier:
-    def __init__(self, args, X_train, X_test, y_train, y_test, sample_weights):
+    def __init__(self, args, X_train, X_test, y_train, y_test):
         self.args = args
         self.X_train = X_train
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
-        self.sample_weights = sample_weights
-        self.tfidf_vectorizer = TfidfVectorizer(min_df=args.min_df, max_df=args.max_df, ngram_range=(1,2), max_features=1000, sublinear_tf=True, norm='l2', use_idf=True, smooth_idf=True)
-        self.X_train_tfidf = self.tfidf_vectorizer.fit_transform(self.X_train)
-        self.X_test_tfidf = self.tfidf_vectorizer.transform(self.X_test)
+        # self.X_train_tfidf = self.tfidf_vectorizer.fit_transform(self.X_train)
+        # self.X_test_tfidf = self.tfidf_vectorizer.transform(self.X_test)
         # print("Sampling starting")
         # print(self.X_train_tfidf.shape, self.y_train.shape, self.y_train.value_counts())
         #Solve class imbalance
@@ -96,7 +94,7 @@ class Classifier:
             }
         scoring = make_scorer(lambda x,y : (f1_score(x,y,average='micro')+f1_score(x,y,average='macro'))/2, greater_is_better=True)
         if (args.model == 'nb'): self.model = MultinomialNB()
-        elif (args.model == 'lr'): self.model = LogisticRegression(C = 1, solver='liblinear', penalty="l2")
+        elif (args.model == 'lr'): self.model = LogisticRegression(C=1.0, class_weight='balanced', solver='liblinear', multi_class='ovr')
         elif (args.model == 'rf'): self.model = RandomForestClassifier()
         elif (args.model == 'svm'): self.model = LinearSVC()
         elif (args.model == 'knn'): self.model = KNeighborsClassifier()
@@ -107,10 +105,10 @@ class Classifier:
         else: raise Exception('Invalid model')
     
     def train(self):
-        self.model.fit(self.X_train_tfidf, self.y_train, sample_weight=self.sample_weights)
+        self.model.fit(self.X_train, self.y_train)
 
     def predict(self):
-        self.y_pred = self.model.predict(self.X_test_tfidf)
+        self.y_pred = self.model.predict(self.X_test)
 
     def evaluate(self):
         predicted_categories = self.y_pred
@@ -127,16 +125,17 @@ class Classifier:
 if __name__ == '__main__':
     args = parse_args().parse_args()
     print(args)
-    # Loader = DataLoader(args)
-    # train_df = Loader.load_data()
+    Loader = DataLoader(args)
+    train_df = Loader.load_data()
 
-    # text_preprocessor = TextPreprocessor(train_df, args)
-    # # text_preprocessor.preprocess()
+    text_preprocessor = TextPreprocessor(train_df, args)
+    X, y = text_preprocessor.preprocess()
+    print("Pre processing done")
     # train_df.to_csv(args.out_file, index=False)
 
-    # print(train_df.head(5))
-    # X_train, X_test, y_train, y_test, sample_weights = Loader.test_train_split(train_df, args.test_size)
-    # classifier = Classifier(args, X_train, X_test, y_train, y_test, sample_weights)
-    # classifier.train()
-    # classifier.predict()
-    # classifier.evaluate()
+    print(train_df.head(5))
+    X_train, X_test, y_train, y_test= Loader.test_train_split(X, y, args.test_size)
+    classifier = Classifier(args, X_train, X_test, y_train, y_test)
+    classifier.train()
+    classifier.predict()
+    classifier.evaluate()
