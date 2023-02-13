8a5898aefca176f3ec5c9e8788c2b33a5b803098
Pratyush Saini <66582958+pratyushiitd@users.noreply.github.com>
2023-02-13 21:34:47 +0530

    running scripts

--- Assignment-1/run.sh
+++ Assignment-1/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python3 src/main_sent.py \
+python3 src/main.py \
     --train_file data/train.csv \
-    --lemmatize=0 \
+    --lemmatize=1 \
     --stem=0 \
     --remove_stopwords=1 \
     --test_size 0.2 \
@@ -12,4 +12,5 @@ python3 src/main_sent.py \
     --ngrams 2 \
     --min_df 5 \
     --max_df 0.95 \
+    --tfidf_max_feat 50000 \
 
